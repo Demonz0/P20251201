630cf4f0b44d54bc0b4312d861a88ab0f80a6379
Takács Imre <takacsi0811@gmail.com>
2025-12-01 13:44:43 +0100

    Add project files.

---
+++ P20251201/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace P20251201
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
